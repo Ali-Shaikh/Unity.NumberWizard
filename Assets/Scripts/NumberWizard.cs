d30961355de03c36ac035f825a0def838d4ac092
Ali-Shaikh <aliakbarshaikh@live.co.uk>
2016-04-03 23:01:00 +0100

    Built logic for the computer to guess the number

--- Assets/Scripts/NumberWizard.cs
+++ Assets/Scripts/NumberWizard.cs
@@ -4,22 +4,28 @@ using System.Collections;
 public class NumberWizard : MonoBehaviour
 {
 
+	int max = 1000;
+	int min = 1;
+	int guess = 500;
+
 	// Use this for initialization
 	void Start ()
 	{
+		max = max + 1;
+		
 		print ("Welcome to Number Wizard");
 		print ("Pick a number in your head, but don't tell me!");
-		
-		int max = 1000;
-		int min = 1;
 		
 		print ("The highest number you can pick is " + max);
 		print ("The lowest number you can pick is " + min);
 		
 		//print ("Is the number higher or lower than 500?");
+		print ("Is the number higher or lower than " + guess + "?");
+		
 		
 		//Using division to get the half of max dynamically
-		print ("Is the number higher or lower than " + max / 2 + "?");
+		//print ("Is the number higher or lower than " + max / 2 + "?");
+		
 		print ("UP = highr, DOWN = lower, RETURN = equal");
 		
 		//Input
@@ -30,9 +36,15 @@ public class NumberWizard : MonoBehaviour
 	void Update ()
 	{
 		if (Input.GetKeyDown (KeyCode.UpArrow)) {
-			print ("UP arrow pressed");
+			min = guess;
+			guess = (max + min) / 2;
+			print ("Higher or lower than " + guess + "?");		
+			
 		} else if (Input.GetKeyDown (KeyCode.DownArrow)) {
-			print ("DOWN arrow pressed");
+			max = guess;
+			guess = (max + min) / 2;
+			print ("Higher or lower than " + guess + "?");
+			
 		} else if (Input.GetKeyDown (KeyCode.Return)) {
 			print ("I WON!");
 		}
